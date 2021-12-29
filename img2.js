9b0bdf565802e2a7cca338caca56dcb54284e3bb
Mr AP <83350206+Mr-AP48@users.noreply.github.com>
2021-12-29 16:48:13 +0530

    Add files via upload

---
+++ img2.js
@@ -0,0 +1,46 @@
+objects = []
+img = ""
+stats = ""
+
+function preload() {
+    img = loadImage('img2.jpg')
+    console.warn("Images Loaded!")
+}
+
+function setup() {
+    canvas = createCanvas(600, 400)
+    canvas.center()
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded)
+    document.getElementById("status").innerHTML = "Status: Detecting Objects"
+}
+
+function modelLoaded() {
+    console.warn("Model Loaded!!!!!!!")
+    stats = true;
+    objectDetector.detect(img, gotResult)
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error)
+    }
+    console.warn(results)
+    objects = results
+}
+
+function draw() {
+    image(img, 0, 0, 600, 400)
+
+    if (stats != "") {
+        for (x = 0; x < objects.length; x++) {
+            document.getElementById("status").innerHTML = "Staus: Object Detected"
+            fill("#FF0000")
+            percent = floor(objects[x].confidence * 100)
+            text(objects[x].label + " " + percent + "%", objects[x].x + 15, objects[x].y + 15)
+            noFill()
+            stroke("#FF0000")
+            rect(objects[x].x, objects[x].y, objects[x].width, objects[x].height)
+        }
+    }
+}
---
+++ main.js
@@ -0,0 +1,8 @@
+function image1() {
+    window.location = "img1.html"
+}
+
+function image2() {
+    window.location = "img2.html"
+    console.log("jhifeguugtjurh")
+}
